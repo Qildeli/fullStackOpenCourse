82dde71a6d8ded348f85b488046d47220130e705
Qildeli <anaqildeli@gmail.com>
2023-10-26 22:13:46 +0400

    Added 2.11.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import SearchFilter from './components/SearchFilter';
 import PersonForm from './components/PersonForm';
 import PersonsList from './components/PersonsList';
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([])
 
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
@@ -41,6 +37,18 @@ const App = () => {
     const filteredPersons = persons.filter(person =>
         person.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
+
+    // Using an Effect hook to fetch initial data from the server
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
 
     return (
         <div>
